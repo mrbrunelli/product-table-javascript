2b09b69c813adac5787c9f351eb4cd7b263227a5
mrbrunelli <matheus.brunelli@gmail.com>
2019-08-29 11:16:33 -0300

    primeiro push com remote add

---
+++ data/products.js
@@ -0,0 +1,35 @@
+const data=[
+    {
+        id:'1',
+        product:'Note Book Acer',
+        brand:'Acer',
+        price:'2657.00',
+        description:'Lorem Ipsum blablbabla',
+        image:'../img/notebook.jpg'
+    },
+    {
+        id:'2',
+        product:'Tenis',
+        brand:'Misuno',
+        price:'235.00',
+        description:'Lorem Ipsum blablbabla',
+        image:'../img/tenis.jpg'
+    },
+    {
+        id:'3',
+        product:'TV Led',
+        brand:'HP',
+        price:'1200.00',
+        description:'Lorem Ipsum blablbabla',
+        image:'../img/tv.jpg'
+    },
+    {
+        id:'4',
+        product:'TV Led',
+        brand:'Sangsum',
+        price:'1200.00',
+        description:'Lorem Ipsum blablbabla',
+        image:'../img/tv.jpg'
+    }
+
+]
